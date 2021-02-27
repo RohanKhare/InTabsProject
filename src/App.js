3a6ce18ffc6f7a10abf495ce63fed64b7dad6cbe
RohanKhare <53505979+RohanKhare@users.noreply.github.com>
2021-02-27 08:24:39 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Component from './Components';
+
+function App() {
+  return (
+    <div className="App">
+      <Component></Component>
+    </div>
+  );
+}
+
+export default App;
