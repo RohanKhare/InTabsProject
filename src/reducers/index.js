fc72713324dd1d3c4c408a5a141fe0bff58861be
RohanKhare <53505979+RohanKhare@users.noreply.github.com>
2021-02-27 08:38:20 +0530

    first

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,15 @@
-initiall
+import { combineReducers } from 'redux';
+
+
+const userData = (initialData = [], action) => {
+    if (action.type === 'SET_USER_DATA') {
+        return action.payload
+    }
+    return initialData
+}
+
+export default combineReducers(
+    {
+        userData: userData
+    }
+);
